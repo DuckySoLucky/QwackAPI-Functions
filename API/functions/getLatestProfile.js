29160f6c280f45e1c62ff53529a83d6dc0568613
DuckySoLucky <robertkovac160@gmail.com>
2022-11-20 11:39:28 -0800

    Update getLatestProfile.js

--- API/functions/getLatestProfile.js
+++ API/functions/getLatestProfile.js
@@ -30,7 +30,7 @@ async function getLatestProfile(uuid) {
 
         if (result.length == 0) return ({ status: 404, reason: `Found no SkyBlock profiles for a user with a UUID of '${uuid}'.` });
 
-        const respond = result.sort((a, b) => b.last_save - a.last_save);
+        const respond = result.find((a) => a.selected);
         const profileData = profileRes.data.profiles.find((a) => a.members[uuid].last_save === respond[0].last_save);
 
         return { status: 200, profile: respond[0], profileData: profileData, playerRes: playerRes.data, player: player, uuid: uuid }
